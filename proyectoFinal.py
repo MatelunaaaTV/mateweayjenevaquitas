28fc88f363a879486b6b6ea85701968dd1fc48c0
MatelunaaaTV <benjaminmatelunaavila11@gmail.com>
2023-06-14 14:44:21 -0400

    precios-prints

---
+++ proyectoFinal.py
@@ -0,0 +1,21 @@
+ANormal = 78900
+AVip = 240000
+
+
+print("Ingrese su nombre.")
+NombreP = (input(""))
+print("Ingrese su rut, con guiones, etc.")
+RutP = (input(""))
+print("Ingrese su numero de telefono, sin digito verificador.")
+NumeroP = int(input(""))
+print("Ingrese de que banco pertenece su cuenta.")
+BancoP = int(input(""))
+print("1. Banco DUOC")
+print("2. Banco Estado")
+print("3. Banco Falabella")
+print("4. Otro")
+
+print("Bienvenido a AeroMax")
+print("Elija una opcion, porfavor")
+print("1. Ver asientos disponibles")
+print("2. Comprar Asiento")
